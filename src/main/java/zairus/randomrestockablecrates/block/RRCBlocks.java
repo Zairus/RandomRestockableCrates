b37e14cf0a4e0965f80c02e0ecfc5597876aaa85
WinterGrave <paulmongeurquijo@gmail.com>
2017-08-09 21:23:56 -0700

    Prepare for 1.11.2

--- src/main/java/zairus/randomrestockablecrates/block/RRCBlocks.java
+++ src/main/java/zairus/randomrestockablecrates/block/RRCBlocks.java
@@ -11,25 +11,25 @@ import zairus.randomrestockablecrates.tileentity.TileEntityCrate;
 
 public class RRCBlocks
 {
-	public static final Block crate;
-	public static final Block crate2;
-	public static final Block crate3;
-	public static final Block crate4;
+	public static final Block CRATE;
+	public static final Block CRATE2;
+	public static final Block CRATE3;
+	public static final Block CRATE4;
 	
 	static
 	{
-		crate = new BlockCrate(Material.WOOD, 0).setSoundType(SoundType.WOOD).setRegistryName(new ResourceLocation(RRCConstants.MODID, "BlockCrate1")).setUnlocalizedName("BlockCrate1");
-		crate2 = new BlockCrate(Material.WOOD, 1).setSoundType(SoundType.WOOD).setRegistryName(new ResourceLocation(RRCConstants.MODID, "BlockCrate2")).setUnlocalizedName("BlockCrate2");
-		crate3 = new BlockCrate(Material.ANVIL, 2).setSoundType(SoundType.METAL).setRegistryName(new ResourceLocation(RRCConstants.MODID, "BlockCrate3")).setUnlocalizedName("BlockCrate3");
-		crate4 = new BlockCrate(Material.ANVIL, 3).setSoundType(SoundType.METAL).setRegistryName(new ResourceLocation(RRCConstants.MODID, "BlockCrate4")).setUnlocalizedName("BlockCrate4");
+		CRATE = new BlockCrate(Material.WOOD, 0).setSoundType(SoundType.WOOD).setRegistryName(new ResourceLocation(RRCConstants.MODID, "blockcrate1")).setUnlocalizedName("blockcrate1");
+		CRATE2 = new BlockCrate(Material.WOOD, 1).setSoundType(SoundType.WOOD).setRegistryName(new ResourceLocation(RRCConstants.MODID, "blockcrate2")).setUnlocalizedName("blockcrate2");
+		CRATE3 = new BlockCrate(Material.ANVIL, 2).setSoundType(SoundType.METAL).setRegistryName(new ResourceLocation(RRCConstants.MODID, "blockcrate3")).setUnlocalizedName("blockcrate3");
+		CRATE4 = new BlockCrate(Material.ANVIL, 3).setSoundType(SoundType.METAL).setRegistryName(new ResourceLocation(RRCConstants.MODID, "blockcrate4")).setUnlocalizedName("blockcrate4");
 	}
 	
 	public static void init()
 	{
-		RandomRestockableCrates.proxy.registerBlock(crate, ((BlockCrate)crate).getModName());
-		RandomRestockableCrates.proxy.registerBlock(crate2, ((BlockCrate)crate2).getModName());
-		RandomRestockableCrates.proxy.registerBlock(crate3, ((BlockCrate)crate3).getModName());
-		RandomRestockableCrates.proxy.registerBlock(crate4, ((BlockCrate)crate4).getModName());
+		RandomRestockableCrates.proxy.registerBlock(CRATE, ((BlockCrate)CRATE).getModName());
+		RandomRestockableCrates.proxy.registerBlock(CRATE2, ((BlockCrate)CRATE2).getModName());
+		RandomRestockableCrates.proxy.registerBlock(CRATE3, ((BlockCrate)CRATE3).getModName());
+		RandomRestockableCrates.proxy.registerBlock(CRATE4, ((BlockCrate)CRATE4).getModName());
 		
 		GameRegistry.registerTileEntity(TileEntityCrate.class, "tileEntityCrate");
 		RandomRestockableCrates.proxy.initTESR();
@@ -37,9 +37,9 @@ public class RRCBlocks
 	
 	public static void initModels()
 	{
-		RandomRestockableCrates.proxy.registerBlockModel(crate, 0, ((BlockCrate)crate).getModName());
-		RandomRestockableCrates.proxy.registerBlockModel(crate2, 0, ((BlockCrate)crate2).getModName());
-		RandomRestockableCrates.proxy.registerBlockModel(crate3, 0, ((BlockCrate)crate3).getModName());
-		RandomRestockableCrates.proxy.registerBlockModel(crate4, 0, ((BlockCrate)crate4).getModName());
+		RandomRestockableCrates.proxy.registerBlockModel(CRATE, 0, ((BlockCrate)CRATE).getModName());
+		RandomRestockableCrates.proxy.registerBlockModel(CRATE2, 0, ((BlockCrate)CRATE2).getModName());
+		RandomRestockableCrates.proxy.registerBlockModel(CRATE3, 0, ((BlockCrate)CRATE3).getModName());
+		RandomRestockableCrates.proxy.registerBlockModel(CRATE4, 0, ((BlockCrate)CRATE4).getModName());
 	}
 }
